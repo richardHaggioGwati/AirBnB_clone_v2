2f5b6f052fc66f3d6b11c627c19bb90922c9de4a
richardHaggioGwati <haggiog0844@gmail.com>
2023-08-23 15:13:07 +0200

    base_module

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@ import uuid
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, DATETIME
+from models import storage
 from models import HBNB_TYPE_STORAGE
 
 Base = declarative_base()
@@ -30,7 +31,7 @@ class BaseModel(Base):
                         default=datetime.utcnow())
 
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
+        """Instantiates a new model"""
         if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -73,5 +74,4 @@ class BaseModel(Base):
 
     def delete(self):
         '''deletes the current instance from the storage'''
-        from models import storage
         storage.delete(self)
